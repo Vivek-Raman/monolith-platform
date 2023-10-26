f77d335f065b2a0e9eeadb4ded4c8066b4b7da26
Vivek Raman <vr.ac4bf@live.com>
2023-10-26 14:06:27 +0530

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-MY_PORT=9000
-FILE_PATH_3_0=./target/api-spec-3-0.json
-curl http://localhost:$MY_PORT/v3/api-docs -o $FILE_PATH_3_0
-
-# convert to openAPI 2.0
-FILE_PATH_2_0=./target/api-spec-2-0.yaml
-api-spec-converter --from=openapi_3 --to=swagger_2 --syntax=yaml $FILE_PATH_3_0 > $FILE_PATH_2_0
