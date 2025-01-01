05eb3a56bc0e8c749f0bc1e7317022a98ff5d062
Vivek Raman <vr.ac4bf@live.com>
2025-01-01 00:15:05 -0800

    Update notion-assistant module

--- src/main/java/dev/vivekraman/monolith/platform/MonolithPlatform.java
+++ src/main/java/dev/vivekraman/monolith/platform/MonolithPlatform.java
@@ -2,10 +2,16 @@ package dev.vivekraman.monolith.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(scanBasePackages = "dev.vivekraman.*")
+@SpringBootApplication(scanBasePackages = "dev.vivekraman.*",
+  exclude = {
+    ReactiveSecurityAutoConfiguration.class,
+    ReactiveUserDetailsServiceAutoConfiguration.class,
+  })
 public class MonolithPlatform {
 	public static void main(String[] args) {
 		SpringApplication.run(MonolithPlatform.class, args);
